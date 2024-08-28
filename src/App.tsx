85e2c8c9dbe98da2849fa21a340a8d58b272d98f
msaufi2325 <msaufi2325@gmail.com>
2024-08-28 21:47:21 +0900

    Add increment and decrement functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import './App.css'
 import { useCounterStore } from './store'
 
 function App() {
-  const [count] = useCounterStore(state => [state.count])
+  const count = useCounterStore((state) => state.count);
 
   return <OtherComponent count={count} />
 }
 
 const OtherComponent = ({ count }: { count: number }) => {
-  return <div>{count}</div>
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
+  return (
+    <div>
+      {count}
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/store.ts
+++ src/store.ts
@@ -2,8 +2,16 @@ import { create } from 'zustand';
 
 type CounterStore = {
   count: number;
+  increment: () => void;
+  decrement: () => void;
 }
 
-export const useCounterStore = create<CounterStore>(() => ({
-  count: 7,
+export const useCounterStore = create<CounterStore>((set) => ({
+  count: 0,
+  increment: () => {
+    set({ count: 1});
+  },
+  decrement: () => {
+    set({ count: -1});
+  },
 }))
